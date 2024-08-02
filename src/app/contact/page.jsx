b70bfbf426fb4758984b2e6d1f9555cdc733bcea
Kaleelur Rahman <kaleel.rahman915@gmail.com>
2024-08-02 18:16:19 +0530

    contact us

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,30 +1,51 @@
-import React from 'react'
-
-function Name({children}){
-    return(
-        <p className='text-2xl font-medium my-2'>{children}</p>
-    )
-}
+import Button from "@/components/Button";
 
 export default function Contact() {
-  return (
-    <div className='text-white'>
-        <h1 className='text-4xl font-semibold'>Our Team</h1>
-        <div>
-            <Name>
-            Susara Ouchithya
-            </Name>
-            <Name>
-            Sahan Madhushanka
-            </Name>
-            <Name>
-            Kaleelur Rahman
-            </Name>
+    return (
+      <div className="min-h-screen flex items-center justify-center p-5">
+        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+          <h2 className="text-2xl font-bold mb-5 text-gray-800">Contact Us</h2>
+          <form>
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2" htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                required
+                className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+                placeholder="Your Name"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2" htmlFor="email">Email</label>
+              <input
+                type="email"
+                required
+                id="email"
+                className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+                placeholder="Your Email"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2" htmlFor="message">Message</label>
+              <textarea
+                id="message"
+                className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+                rows="4"
+                required
+                placeholder="Your Message"
+              ></textarea>
+            </div>
+            {/* <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-3 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
+            >
+              Send Message
+            </button> */}
+            <Button label={'Send Message'} className="w-full" type={"submit"} />
+          </form>
         </div>
-        <div>
-            <p className='text-3xl font-medium'>Supervised By:</p>
-            <p>Suzorro Daniel</p>
-        </div>
-    </div>
-  )
-}
+      </div>
+    )
+  }
+  
